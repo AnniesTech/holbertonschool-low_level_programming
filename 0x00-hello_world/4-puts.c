5e581cd86dbbb0395a848b1602490e1658306b9f
Ana Rocha <anita.rocha0505@gmail.com>
2021-09-30 13:46:14 -0500

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-#include <string.h>
+#include <sdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 <Success>
+ */
 
 int main(void)
 {
-	char str1[100];
-		strcpy(str1, "\"Programming is like building a multilingual puzzle ");
-		puts(str1);
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
 }
