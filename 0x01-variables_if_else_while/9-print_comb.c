0cb3f59b85d3315644c8f05ae37b9707387ca48d
Ana Rocha <anita.rocha0505@gmail.com>
2021-10-01 18:21:42 -0500

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 <Success>
+ */
+
+
+int main(void)
+{
+char n;
+
+for (n = '0'; n <= '9'; n++)
+{
+	putchar(n);
+	if (n != '9')
+	{
+		putchar(44);
+		putchar(32);
+	}
+}
+	putchar(10);
+	return (0);
+}
