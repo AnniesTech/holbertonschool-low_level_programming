9e68737a7998afcd4e6e7005a0bfeb5cdcc1e3a8
Ana Rocha <anita.rocha0505@gmail.com>
2021-10-01 16:25:31 -0500

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 <Success>
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'Z'; ch++)
+	putchar(ch);
+	putchar(10);
+	return (0);
+}
