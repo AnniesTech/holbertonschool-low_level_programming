71f98a5e89da434808e631229401a6c78c828896
Ana Rocha <anita.rocha0505@gmail.com>
2021-10-04 15:09:07 -0500

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 <Success>
+ */
+
+int main(void)
+{
+	char *put = "_putchar\n";
+
+	while (*put)
+	putchar(*put++);
+
+	return (0);
+}
