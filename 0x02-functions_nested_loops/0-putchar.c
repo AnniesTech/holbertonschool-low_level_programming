2bcdf521045628a1b3352f4305d526cc1ffd1707
Ana Rocha <anita.rocha0505@gmail.com>
2021-10-05 09:12:29 -0500

    Correction point 0, extra line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,12 @@
 
 int main(void)
 {
-	char output[] = "_putchar";
+	char output[] = "_putchar\n";
 	int a = 0;
 
 	for (a = 0; a < 9; a++)
 	{
 		putchar(output[a]);
 	}
-
-	putchar(10);
 	return (0);
 }
