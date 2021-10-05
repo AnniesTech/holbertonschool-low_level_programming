753049d7bebaa63641d35ed41969630cfc87b8b0
Ana Rocha <anita.rocha0505@gmail.com>
2021-10-05 10:59:34 -0500

    corrected 9 point

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,24 +14,29 @@ void times_table(void)
 		{
 			c = a * b;
 
-			if ((c / 10) == 0)
+			if (b != 0)
 			{
-				if (b != 0)
-					_putchar(32);
-				_putchar(c + '0');
-
-				if (b == 9)
-				_putchar(44);
-				_putchar(32);
+			_putchar(44);
+			_putchar(32);
 			}
+
+			if (c >= 10)
+			{
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+			}
+
+			else if (c < 10 && b !=0)
+			{
+			_putchar(32);
+			_putchar((c % 10) + '0');
+			}
+
 			else
 			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-				if (b == 9)
-				_putchar(44);
-				_putchar(32);
+			_putchar((c % 10) + '0');
 			}
+
 		}
 		_putchar(10);
 	}
