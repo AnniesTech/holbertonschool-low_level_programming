c3d381f2a295f65785d4aab3bae188fbd6daadc5
Ana Rocha <anita.rocha0505@gmail.com>
2021-10-05 11:50:52 -0500

    11th point

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *print_to_98 - prints the "n" to 98
+ *@n: variable
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
