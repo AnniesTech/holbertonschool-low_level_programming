c7f775850a9a9cd41e44bd1a3cce977f32f1d555
Ana Rocha <anita.rocha0505@gmail.com>
2021-10-11 17:10:55 -0500

    1 swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
+/**
+ * swap_int - Swaps the values of the varyables
+ * @a: Var #1
+ * @b: Var #2
+ */
+
 void swap_int(int *a, int *b)
 {
-    int c;
+	int c;
 
-    c = *b;
-    *b = *a;
-    *a = c;
+	c = *b;
+	*b = *a;
+	*a = c;
 }
