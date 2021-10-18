566bf533305f79bac7a97b5a582b43da99c38e8e
Ana Rocha <anita.rocha0505@gmail.com>
2021-10-18 14:41:41 -0500

    Correction of betty for 1 memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,20 @@
 #include "main.h"
+/**
+ * _memcpy - function that copies memory area.
+ * @dest: place where it will be copied
+ * @src: information that will be copied
+ * @n: Variable
+ * Return: to dest
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  char *d = dest;
-  const char *s = src;
-  while (n--)
-  {
-    *d++ = *s++;
-  }
-  return (dest);
+	char *d = dest;
+	const char *s = src;
+
+	while (n--)
+	{
+		*d++ = *s++;
+	}
+	return (dest);
 }
