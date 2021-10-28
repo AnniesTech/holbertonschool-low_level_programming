c80e08e42b168266947e88436f6169f950101b8b
Ana Rocha <anita.rocha0505@gmail.com>
2021-10-28 14:10:41 -0500

    added a new variable k

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l1, l2, i, j;
+	unsigned int l1, l2, i, j, k = 0;
 	char *s;
 
 	if (s1 == NULL)
@@ -30,8 +30,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];
-	for (j = l1; j < l1 + n; j++)
-		s[j] = s2[j - l1];
+	for (j = l1; j < l1 + n; j++, k++)
+		s[j] = s2[k];
 	s[j] = '\0';
 	return (s);
 }
