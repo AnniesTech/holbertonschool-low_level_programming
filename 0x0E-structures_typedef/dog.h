58151c26d8cd2713bb55d616c259c9bec2f87266
Ana Rocha <anita.rocha0505@gmail.com>
2021-11-01 15:05:17 -0500

    Dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - Short description
+ * @name: Name of the doggo
+ * @age: Age of the doggo
+ * @owner: Owner of the doggo
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
