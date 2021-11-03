279eb171f83393b77438a5e379483e43628b9a02
Ana Rocha <anita.rocha0505@gmail.com>
2021-11-03 16:48:03 -0500

    Calculator, output was correct

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stddef.h>
+
+/**
+ * get_op_func - gets the parameter entered in
+ * the console by the user and compares it with the operands functions
+ * @s: Value entered in the console
+ * Return: NULL, if the signed entered doesn't match
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*s == *ops[i].op)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
