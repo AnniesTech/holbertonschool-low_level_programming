2c52690d180950cb012afed68db03727c58b1f31
AnniesTech <anita.rocha0505@gmail.com>
2022-01-06 12:00:32 -0500

    1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * dlistint_len -  function that returns the number of
+ * elements in a linked dlistint_t list.
+ *
+ *@h: head of the list
+ *Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int contador = 0;
+
+	if (h == NULL)
+		return (contador);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		contador++;
+	}
+	return (contador);
+}
